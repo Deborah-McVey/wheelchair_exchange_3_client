3232c2bc18dfa31bffdc523f4c3f1aaab1d31f78
Deborah McVey <deborah.mcvey@gmail.com>
2024-03-21 20:45:47 -0400

    login component

---
+++ src/app/features/post-new/post-new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-new.component.html',
+  styleUrl: './post-new.component.scss'
+})
+export class PostNewComponent {
+
+}
